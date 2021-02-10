262222cdbfd09172149a9a3569f1cb0960e69c0c
OskariHanninen <76906268+OskariHanninen@users.noreply.github.com>
2021-02-10 22:39:41 +0200

    Update events.h

--- Box/events.h
+++ Box/events.h
@@ -6,8 +6,8 @@ enum EventType {
   LED_ON,
   LED_OFF,
   BLINK,
-  BLINK_ON,
-  BLINK_OFF,
+  BLINK_ON = 5,
+  BLINK_OFF = 6,
   BTN_SHORT,
   BTN_LONG,
   COLOR
@@ -18,3 +18,46 @@ typedef struct Event
   EventType type;
   int data;
 } Event;
+
+String eventToString(Event e)
+{
+  String s = "";
+  switch(static_cast<int>(e.type))
+  {
+    case UNKNOWN:
+      s = "UNKNOWN";
+      break;
+    case LED:
+      s = "LED";
+      break;
+    case LED_ON:
+      s = "LED_ON";
+      break;
+    case LED_OFF:
+      s = "LED_OFF";
+      break;
+    case BLINK:
+      s = "BLINK_ON";
+      break;
+    case BLINK_ON:
+      s = "BLINK_ON";
+      break;
+    case BLINK_OFF:
+      s = "BLINK_OFF";
+      break;
+    case BTN_SHORT:
+      s = "BTN_SHORT";
+      break;
+    case BTN_LONG:
+      s = "BTN_LONG";
+      break;
+    case COLOR:
+      s = "COLOR";
+      break;
+    default:
+      s = "DEFAULT";
+      break;
+  }
+
+  return s;
+}
